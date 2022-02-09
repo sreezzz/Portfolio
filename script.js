861c4ed726d3251fe2dd9ceef0aed147e0dc0a75
Sreedhar Vangala <sridharvangala741@gmail.com>
2022-02-09 09:12:20 +0530

    Changed commit

--- script.js
+++ script.js
@@ -50,11 +50,11 @@ $(document).ready(function(){
 
     // owl carousel script
     $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
+        margin: 30,
+        // loop: true,
+        // autoplay: true,
         autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
+        // autoplayHoverPause: true,
         responsive: {
             0:{
                 items: 1,
